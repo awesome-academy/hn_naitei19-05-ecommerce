86f5d229734eed4fa1d68f1db563d789e1ff5054
phtchung <phamthanhchung2001@gmail.com>
2023-09-19 17:14:56 +0700

    filter product by category and price

--- src/main/java/com/example/naitei19javaecommerce/controller/user/HomeController.java
+++ src/main/java/com/example/naitei19javaecommerce/controller/user/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -26,4 +28,28 @@ public class HomeController {
 
         return "user/static-pages/home-page";
     }
+
+    @GetMapping("/shop")
+    public String Shop(Model model) {
+        model.addAttribute("allProducts", productService.findAll());
+        return "user/search-page/index";
+    }
+
+    @PostMapping("/filter")
+    public String filterProducts(Model model,
+                                 @RequestParam(name = "selectedCategories", required = false) List<String> selectedCategories,
+                                 @RequestParam(name = "minValue", required = false) String  minValue,
+                                 @RequestParam(name = "maxValue", required = false) String maxValue ) {
+            Double minPrice = Double.valueOf(minValue.substring(1));
+            Double maxPrice = Double.valueOf(maxValue.substring(1));
+
+            List<Product> filteredResult = productService.filterProducts(selectedCategories,minPrice,maxPrice);
+            if(filteredResult.isEmpty()){
+                model.addAttribute("message","Không tồn tại sản phẩm!");
+            }else {
+                model.addAttribute("filterProducts",filteredResult);
+            }
+        return "user/search-page/index";
+    }
 }
+
--- src/main/java/com/example/naitei19javaecommerce/repository/ProductRepository.java
+++ src/main/java/com/example/naitei19javaecommerce/repository/ProductRepository.java
@@ -1,20 +1,21 @@
 package com.example.naitei19javaecommerce.repository;
 
-import com.example.naitei19javaecommerce.model.Category;
 import com.example.naitei19javaecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     Optional<Product> findByName(String name);
 
     @Query("SELECT p FROM Product p WHERE p.category.id = :categoryId AND p.id <> :productId")
     List<Product> findProductRelatedById(@Param("categoryId") Long categoryId,
-                                             @Param("productId") Long productId);
+                                         @Param("productId") Long productId);
+
     @Query(value = """
             SELECT p.*
                           FROM products p
@@ -23,4 +24,17 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                           ORDER BY sum(i.quantity) DESC limit 5
                          """, nativeQuery = true)
     List<Product> findOutStandingProducts();
+
+    @Query("""
+            select p from Product p where  p.price >:minPrice and p.price <:maxPrice
+            """)
+    List<Product> findAllByPrice(Double minPrice, Double maxPrice);
+
+    @Query("""
+            select p from Product p where
+            (:category IS NULL OR p.category.name =:category ) AND
+            (:minPrice IS NULL OR p.price >:minPrice ) AND
+            (:maxPrice IS NULL OR p.price <:maxPrice )
+            """)
+    List<Product> filterProductByCateAndPrice(String category, Double minPrice, Double maxPrice);
 }
