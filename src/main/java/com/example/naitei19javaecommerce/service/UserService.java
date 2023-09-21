1087bf9f8e8ee3cace4e3ebe27f59aa100723d60
nghiahn-0358 <hoang.nhu.nghia@sun-asterisk.com>
2023-09-21 10:45:32 +0700

    create login user

--- src/main/java/com/example/naitei19javaecommerce/service/UserService.java
+++ src/main/java/com/example/naitei19javaecommerce/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.naitei19javaecommerce.service;
 
+import com.example.naitei19javaecommerce.model.User;
+
 public interface UserService {
+    User findByUsername (String username);
 }
